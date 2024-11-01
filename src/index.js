ecbb984b0b731c74929e19d95e47f76f6c0e8d27
JK932-coder <ekaterina.kosmaciova@gmail.com>
2024-11-01 16:01:17 +0000

    Updated context a little

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function generateRecipe(event) {
 let instructionsInput = document.querySelector("#user-input");
   let apiKey = "43008a6ae95edac1643c1odtb4f58d13";
   let prompt=`User instructions:Generate a recipe about ${instructionsInput.value}`;
-  let context="You are a chef that knows a lot of recipes and love to prepare dishes that are quick to make.Your mission is to generate a very quick recipe in simple HTML. Do not include the word html into your response.Organize the recipe into ingredients and Method.Make sure to follow the user instructions."; 
+  let context="You are a chef that knows a lot of recipes and love to prepare dishes that are quick to make.Your mission is to generate a very quick recipe in simple HTML. Do not include the word html into your response.Organize the recipe into ingredients and Method and include these as labels.Make sure to follow the user instructions."; 
   let apiUrl =`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let recipeElement=document.querySelector("#recipe"); 
